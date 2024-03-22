704b7ee1aed064f094e585543249aff9e1de20c0
RobinP122 <robinpljx@gmail.com>
2024-03-22 14:57:07 +0100

    Added termsOfUse and PrivacyPolicy pages and styling

---
+++ app/legal/terms-of-use/page.tsx
@@ -0,0 +1,93 @@
+"use client"
+import '@/app/styles/pages.css'
+
+export default function TermsOfUse() {
+    return (
+        <div className='w-full flex flex-col align-middle justify-center pt-2 pb-8'>
+            <div className='flex flex-col m-auto'>
+
+                <h1>Terms of Use</h1>
+
+                <p>By accessing the website at <a href="https://explorer.aiblock.ch/">explorer.aiblock.ch</a> you are agreeing to be bound by these terms
+                    of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable
+                    local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The
+                    materials contained in this website are protected by applicable copyright and trademark law.</p>
+
+                <br />
+
+                <h2>Use License</h2>
+
+                <p>These Terms of Use permit you to use the Website for your personal, noncommercial use only. You may not:</p>
+
+                <ul>
+                    <li>modify or copy the materials;</li>
+                    <li>use the materials for any commercial purpose, or for any public display (commercial or noncommercial);</li>
+                    <li>remove any copyright or other proprietary notations from the materials;</li>
+                </ul>
+
+                <p>This license shall automatically terminate if you violate any of these restrictions and may be terminated by A-Block
+                    Technologies AG at any time. Upon termination of this license, you must destroy any materials in your possession,
+                    whether in electronic or printed format.</p>
+                <br />
+
+                <h2>Disclaimer</h2>
+
+                <p>The materials on <a href="https://explorer.aiblock.ch/">explorer.aiblock.ch</a> website are provided on an ‘as is’ basis. A-Block
+                    Technologies AG makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties,
+                    including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose,
+                    or noninfringement of intellectual property or other violation of rights. Further, A-Block Technologies AG does not
+                    warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials
+                    on its website or otherwise relating to such materials or on any sites linked to this site.</p>
+
+                <br />
+
+                <h2>Limitations</h2>
+
+                <p>In no event shall A-Block Technologies AG or its subcontractors be liable for any damages (including, without limitation,
+                    damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the
+                    materials on this website, even if A-Block Technologies AG or a A-Block Technologies AG authorized representative
+                    has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow
+                    limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations
+                    may not apply to you.</p>
+
+                <br />
+
+                <h2>Accuracy of materials</h2>
+
+                <p>The materials appearing on <a href="https://explorer.aiblock.ch/">explorer.aiblock.ch</a> website could include technical, typographical,
+                    or photographic errors. A-Block Technologies AG does not warrant that any of the materials on its website are accurate,
+                    complete or current. A-Block Technologies AG may make changes to the materials contained on its website at any time
+                    without notice. However, A-Block Technologies AG does not make any commitment to updating the materials.</p>
+
+                <br />
+
+                <h2>Links</h2>
+
+                <p>A-Block Technologies AG has not reviewed all of the sites linked to its website and is not responsible for the contents
+                    of any such linked site. The inclusion of any link does not imply endorsement by A-Block Technologies AG of the
+                    site. Use of any such linked website is at the user’s own risk.</p>
+
+                <br />
+
+                <h2>Modifications</h2>
+
+                <p>A-Block Technologies AG may revise these terms of service for its website at any time without notice. By using this
+                    website you are agreeing to be bound by the then current version of these terms of service.</p>
+
+                <br />
+
+                <h2>Governing Law</h2>
+
+                <p>These terms and conditions are governed by and construed in accordance with the laws of Switzerland and you irrevocably
+                    submit to the exclusive jurisdiction of the courts in that State or location.</p>
+
+                <br />
+                
+                <p>This policy is effective as of 1 December 2023.</p>
+
+                <br/>
+
+            </div>
+        </div>
+    )
+}
