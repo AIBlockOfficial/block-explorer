73e9796e3060253d0799fbd0340f6e4b62508088
RobinP122 <robinpljx@gmail.com>
2024-04-12 12:10:10 +0200

    chore: update environment variables - Changed name "WEBSITE" to "OUTLINK" to be more clear - Changed values for testnet env

--- public/__ENV.js
+++ public/__ENV.js
@@ -1,1 +1,1 @@
-window.__ENV = {"NEXT_APP_EXP_BACKEND":"https://explorer-api.aiblock.ch/api","NEXT_APP_NETWORK":"Devnet","NEXT_APP_WEBSITE":"https://explorer.aiblock.ch/","NEXT_APP_PORT":"8080","NEXT_APP_STORAGE_URL":"https://storage.aiblock.ch","REACT_ENV_PREFIX":"NEXT_APP"};
+window.__ENV = {"NEXT_APP_EXP_BACKEND":"http://loki.explorer.se3ker.com/api","NEXT_APP_NETWORK":"Developement","NEXT_APP_OUTLINK":"https://explorer.aiblock.ch/","NEXT_APP_PORT":"8080","NEXT_APP_STORAGE_URL":"https://storage.aiblock.dev","REACT_ENV_PREFIX":"NEXT_APP"};
