5b31d77ef6f1aa9cf7a934ef162f43236947d9f0
RobinP122 <robinpljx@gmail.com>
2024-08-21 13:34:52 +0200

    Add CSP header to nextjs config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,34 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     compiler: { styledComponents: true, },
-    output: "standalone"
+    output: "standalone",
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: cspHeader.replace(/\n/g, ''),
+                    },
+                ],
+            },
+        ]
+    },
+
 }
 
 module.exports = nextConfig
